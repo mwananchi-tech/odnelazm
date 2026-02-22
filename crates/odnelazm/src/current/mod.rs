87d21b49931d893b349bf42f6e5fc93e17ccb4b9
Collins Muriuki <hello@collinsmuriuki.xyz>
2026-02-22 15:47:43 +0300

    chore(WIP): Boilerplate

---
+++ crates/odnelazm/src/current/mod.rs
@@ -0,0 +1,7 @@
+pub(crate) const BASE_URL: &str = "https://mzalendo.com";
+
+// hansard list URL: https://mzalendo.com/democracy-tools/hansard/?page=2
+// hansard sitting URL(national_assembly): https://mzalendo.com/democracy-tools/hansard/wednesday-26th-november-2025-afternoon-sitting-1561/
+// hansard sitting URL(senate): https://mzalendo.com/democracy-tools/hansard/wednesday-26th-november-2025-afternoon-sitting-2012/
+// members URL(national_assembly): https://mzalendo.com/mps-performance/national-assembly/13th-parliament/?q=&page=1/ [13th-11th supported]
+// members URL(senate):https://mzalendo.com/mps-performance/senate/13th-parliament/?q=&page=1/ [13th-11th supported]
